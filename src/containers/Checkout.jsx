454b81ade2602b1665b5cf85bcfd3d3ca68e0fa0
celismx <rioshuamaniluis@gmail.com>
2021-06-18 12:06:28 -0500

    [add] react-helmet

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import AppContext from '../context/AppContext';
 import '../styles/components/Checkout.css';
 
@@ -18,6 +19,10 @@ const Checkout = ( ) => {
 	}
 
 	return ( 
+		<>
+		<Helmet>
+		<title>Lista de pedidos - Platzi Conf Merch</title>
+		</Helmet>
 		<div className="Checkout">
 		<div className="Checkout-content">
 		<h3>Lista de pedidos:</h3>
@@ -46,6 +51,7 @@ const Checkout = ( ) => {
 		</div>
 		)}
 		</div>
+		</>
 	);
 }
 
